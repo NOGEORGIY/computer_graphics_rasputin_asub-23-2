db5df16c98024528f9e8a282fb4a8ea432034e7d
Veryrasputin <118193726+NOGEORGIY@users.noreply.github.com>
2025-02-27 18:33:43 +0800

    До этого кинул старый код
    теперь новый!

--- Rasputin-20-sol_first_lab/Rasputin-20/main.cpp
+++ Rasputin-20-sol_first_lab/Rasputin-20/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+#include <math.h>
 
 int main() {
 	if (!glfwInit()) {
@@ -10,10 +11,10 @@ int main() {
 		return 1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window;
 	window = glfwCreateWindow(1024, 768, "Help", nullptr, nullptr);
@@ -32,18 +33,80 @@ int main() {
 		return 1;
 	}
 
-	while(!glfwWindowShouldClose(window)){
-		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
-		//¬ методичке 1.0, 1.0, 1.0
+	//¬ VBO записываютс€ вершины
+	//VAO нужно дл€ отрисовки
+	//EBO хранит индексы вершин, выбира€ которые можно рисовать фигуры
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	GLfloat points[] = {
+	-0.4f, -0.2f, 0.0f,
+	-0.4f, 0.4f, 0.0f,
+	0.4f, 0.4f, 0.0f,
+	0.4f, -0.2f, 0.0f,
+	};
+
+	GLuint elements[] = {
+	0, 1, 2,
+	2, 3, 0
+	};
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+	const char* vert_shader =
+		"#version 410 core\n"
+		"layout (location=0) in vec3 vp;"
+		"void main() {"
+		"	gl_Position = vec4(vp.x, vp.y, vp.z, 1.0);"
+		"}";
+
+	const char* frag_shader =
+		"#version 410 core\n"
+		"out vec4 frag_colour;"
+		"uniform vec4 in_color;"
+		"void main() {"
+			"frag_colour = in_color;"
+		"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vert_shader, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &frag_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_program = glCreateProgram();
+	glAttachShader(shader_program, fs);
+	glAttachShader(shader_program, vs);
+	glLinkProgram(shader_program);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0); //—ообщает как интерпретировать вершинные данные. 
+	glEnableVertexAttribArray(0); 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
-		
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glBegin(GL_QUADS);
-			glVertex2f(-0.4f, -0.2f); 
-			glVertex2f(-0.4f, 0.4f);
-			glVertex2f(0.4f, 0.4f);
-			glVertex2f(0.4f, -0.2f);
-		glEnd();
+		glUseProgram(shader_program);
+
+		float vertexColorLocation = glGetUniformLocation(shader_program, "in_color");
+
+		float timeValue = glfwGetTime();
+
+		glUniform4f(vertexColorLocation, 1-sin(timeValue), cos(timeValue), sin(timeValue), 1.0f);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		//¬ методичке 1.0, 1.0, 1.0
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
---
+++ Rasputin-20-sol_first_lab/Rasputin-20/shader_loader.h
@@ -0,0 +1,14 @@
+#pragma once
+#define GLEW_DLL
+#define GLFW_DLL
+#include <cstdio>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+class shader_loader {
+	
+public:
+	shader_loader();
+};
